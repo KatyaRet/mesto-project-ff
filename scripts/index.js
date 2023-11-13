05c4c84b035d76f7210405a947140d20d616b37c
Katya Retyunskaya <ekaterina.retiunskaya@yandex.ru>
2023-11-13 16:11:01 +0300

    Первая версия

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const content = document.querySelector('.content');
+const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardTitle, cardImage, deletedCard) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__title').textContent = cardTitle;
+    cardElement.querySelector('.card__image').src = cardImage;
+    cardElement.querySelector('.card__image').alt = cardTitle;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+        deleteButton.addEventListener('click', (evt) => {
+            deletedCard(evt);
+        });
+        return cardElement;
+};
 
 // @todo: Функция удаления карточки
+const deleteCard = (evt) => {
+    evt.target.parentElement.remove();
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function ({name, link}) {
+    const cardData = createCard(name, link, deleteCard);
+    cardList.append(cardData);
+});
