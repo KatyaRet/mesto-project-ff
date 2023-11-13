f421f08ed9ceaa1f90fc7c67b2207b7bc45bc4a6
Katya Retyunskaya <ekaterina.retiunskaya@yandex.ru>
2023-11-13 20:22:26 +0300

    Исправленная версия

--- scripts/index.js
+++ scripts/index.js
@@ -7,12 +7,11 @@ const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 function createCard(cardTitle, cardImage, deletedCard) {
-    
+
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
     cardElement.querySelector('.card__title').textContent = cardTitle;
     cardElement.querySelector('.card__image').src = cardImage;
-    cardImage.alt = cardTitle;
 
     const deleteButton = cardElement.querySelector('.card__delete-button');
         deleteButton.addEventListener('click', (evt) => {
